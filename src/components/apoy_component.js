f21bd6a052e0ecf56268226ee5138aa03d463567
Yoisell Cervantes Almaguer <46346959+ycervantes1985@users.noreply.github.com>
2022-09-29 19:25:49 -0300

    last message

--- src/components/apoy_component.js
+++ src/components/apoy_component.js
@@ -1,61 +1,56 @@
-import React from "react"
+
 import '../App.css';
 
 
-
 const userObject = [
-    {
-        firsName:'Yoisell',
-        lastName: 'Cervantes',
-        age: 37,
-        hairColor:'Black'
-    },
-    {
-      firsName:'Jany',
-      lastName: 'Garcia',
-      age: 35,
-      hairColor:'Black'
-    },
-    {
-      firsName:'Kevin',
+  {
+      firsName:'Yoisell',
       lastName: 'Cervantes',
-      age: 9,
+      age: 37,
       hairColor:'Black'
-    },
-    {
-      firsName:'Alejandro',
-      lastName: 'Pulido',
-      age: 10,
-      hairColor:'Black'
-    }
-  ]
+  },
+  {
+    firsName:'Jany',
+    lastName: 'Garcia',
+    age: 35,
+    hairColor:'Black'
+  },
+  {
+    firsName:'Kevin',
+    lastName: 'Cervantes',
+    age: 9,
+    hairColor:'Black'
+  },
+  {
+    firsName:'Alejandro',
+    lastName: 'Pulido',
+    age: 10,
+    hairColor:'Black'
+  }
+]
+
 
 
   function MyComponent(){
 
-return (
-    <>
-    {userObject.map(({firsName, lastName, age, hairColor}) => (
-     <div className="apoyo">
-      <h1>
-          <strong>{lastName},{firsName}</strong>
-      </h1>
-      <h4>Age:{age}</h4>
-      <h4>Hair Color:{hairColor}</h4>
-
-      </div>
-
-     
-
-        
-        ))}
+   
     
-    </>    
+    return (
+    <>
+    {userObject.map(({firsName, lastName, age, hairColor}, index) => (
+        <div className="apoyo">
+            <h1>
+                <strong>{lastName},{firsName}</strong>
+            </h1>
+            <h4>Age:{age}</h4>
+            <h4>Hair Color:{hairColor}</h4>
+            <div>
+                <button>+</button>
+            </div>
+        </div>
+))}
+    </>
     );
-
-
-
 }
-
 
 export default MyComponent;
